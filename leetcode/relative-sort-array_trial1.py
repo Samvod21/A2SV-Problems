008f8d0ddd40b62e32efdcf286373adc56195c80
Samvod21 <165063578+Samvod21@users.noreply.github.com>
2026-02-27 10:36:32 -0900

    Delete leetcode/relative-sort-array_trial1.py

--- leetcode/relative-sort-array_trial1.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def relativeSortArray(self, arr1, arr2):
-        """
-        :type arr1: List[int]
-        :type arr2: List[int]
-        :rtype: List[int]
-        """
-        

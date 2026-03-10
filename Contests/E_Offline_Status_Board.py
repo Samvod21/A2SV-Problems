9024bb7329b5f74d6068c51e0c04da24e08e0fb7
Samvod21 <SamuelTeshome79@gmail.com>
2026-03-09 22:37:40 -0900

    Add for submit

--- Contests/E_Offline_Status_Board.py
+++ Contests/E_Offline_Status_Board.py
@@ -2,18 +2,18 @@ for _ in range(int(input())):
     n = int(input())
     lst = list(map(int,input().split()))
     lst2 = list(map(int,input().split()))
-    dict1 = {v:i for i,v in enumerate(lst)}
+    dict1 = {v:i for i, v in enumerate(lst)}
 
     ptr = n-1
     ans = n-1
 
-    while ans>=0:
-        if dict1[lst2[ans]]>ptr:
+    while ans >= 0:
+        if dict1[lst2[ans]] >ptr:
             break
 
         ptr = dict1[lst2[ans]]
         ans -= 1
     
-    print(ans+1)
+    print(ans + 1)
 
 

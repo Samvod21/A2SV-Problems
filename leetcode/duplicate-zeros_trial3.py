2ed93a920a54889584f1bd075333844b47b636b9
Samvod21 <165063578+Samvod21@users.noreply.github.com>
2026-03-15 04:14:41 -0900

    Add solution for Duplicate Zeros (trial 3)

---
+++ leetcode/duplicate-zeros_trial3.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        i = 0
+
+        while i < len(arr): 
+            if arr[i] == 0:
+                arr.insert(i + 1, 0)
+                arr.pop()
+                i += 1
+
+            i += 1      

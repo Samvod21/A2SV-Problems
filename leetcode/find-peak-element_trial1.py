6da7a3bcf0ddf69dea6289bae86e44cb80caee69
Samvod21 <165063578+Samvod21@users.noreply.github.com>
2026-04-22 21:08:51 +0300

    Add solution for Find Peak Element (trial 1)

---
+++ leetcode/find-peak-element_trial1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            
+            else:
+                l = mid + 1
+        
+        return r
+        
